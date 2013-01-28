2aeefe3df35defe579a58ee735aa913ed3505d74
Fylwind <fylwind314@gmail.com>
2013-01-28 05:50:47 -0500

    Allow add-on to be enabled/disabled.

--- FZXC.lua
+++ FZXC.lua
@@ -44,6 +44,7 @@ local string_format = string.format
 local string_lower = string.lower
 local string_gmatch = string.gmatch
 local string_gsub = string.gsub
+local string_match = string.match
 local string_sub = string.sub
 
 local BNGetFriendInfo = BNGetFriendInfo
@@ -59,6 +60,7 @@ local UnitFactionGroup = UnitFactionGroup
 local FZMP = FZMP
 local FZMP_SendMessage = FZMP.SendMessage
 local FZMP_RegisterMessageListener = FZMP.RegisterMessageListener
+local FZMP_UnregisterMessageListener = FZMP.UnregisterMessageListener
 
 local playerName = UnitName("player")
 local playerRealm = GetRealmName()
@@ -66,6 +68,7 @@ local playerFaction
 
 local version = "@project-version@"
 local timestamp = tonumber("@project-timestamp@")
+local frame = CreateFrame("Frame")
 local sources
 local messageArchive = {}
 
@@ -74,6 +77,10 @@ local L = function(...) return ... end
 
 local function cprint(text)
     DEFAULT_CHAT_FRAME:AddMessage(text, .7, 1, .4)
+end
+
+local function cprintf(format, ...)
+    cprint(string_format(format, ...))
 end
 
 local function updateSources()
@@ -152,9 +159,7 @@ local function checkVersion(data, data2)
     end
     local otherTimestamp = tonumber(data2)
     if timestamp and otherTimestamp and timestamp < otherTimestamp then
-        cprint(string_format(
-                   L("fzxc: a new version (%s) is available."),
-                   data))
+        cprintf(L"fzxc: a new version (%s) is available.", data)
         versionChecked = true
     end
 end
@@ -194,6 +199,7 @@ local function receiveTransmission(_, data, _, presenceID)
     local counter, sender, channel, text, realm, faction = unpack(data)
     if channel == "" then
         replyMessage(presenceID, unpack(data))
+        return
     end
     if not realm then                   -- Backward compatibility: previous
         local _                         -- versions didn't send realm & faction
@@ -222,29 +228,6 @@ local function receiveTransmission(_, data, _, presenceID)
                                       sender, string_sub(fullText, 256)),
                         "CHANNEL", nil, channelNum)
     end
-end
-
-local frame = CreateFrame("Frame")
-
-local function onPlayerEnteringWorld()
-    frame:UnregisterEvent("PLAYER_ENTERING_WORLD")
-    playerFaction = UnitFactionGroup("player")
-    updateSources()
-    local connectedPresenceIDs = {}
-    for source, dests in pairs(sources) do
-        for dest, presenceIDs in pairs(dests) do
-            for presenceID, info in pairs(presenceIDs) do
-                if info.connected then
-                    connectedPresenceIDs[presenceID] = true
-                end
-            end
-        end
-    end
-    for presenceID, _ in pairs(connectedPresenceIDs) do
-        FZMP_SendMessage("FZX", {VERSION_REQUEST, version, "", timestamp},
-                         "BN_WHISPER", presenceID)
-    end
-    FZMP_RegisterMessageListener("FZX", receiveTransmission)
 end
 
 local function onChatMsgChannel(text, sender, _, _, _, _, _,
@@ -329,85 +312,201 @@ local function onUpdate(_, elapsed)
         end
     end
 end
-frame:SetScript("OnUpdate", onUpdate)
 
-frame:RegisterEvent("CHAT_MSG_CHANNEL")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-local function onEvent(_, event, ...)
-    if event == "PLAYER_ENTERING_WORLD" then
-        onPlayerEnteringWorld(...)
-    elseif event == "CHAT_MSG_CHANNEL" then
-        onChatMsgChannel(...)
-    end
+local function enable()
+    FZXC_DB.disabled = nil
+    frame:RegisterEvent("CHAT_MSG_CHANNEL")
+    FZMP_RegisterMessageListener("FZX", receiveTransmission)
 end
-frame:SetScript("OnEvent", onEvent)
 
-SLASH_FZXC1 = "/fzxc"
-function SlashCmdList.FZXC(str)
+local function disable()
+    FZXC_DB.disabled = true
+    frame:UnregisterEvent("CHAT_MSG_CHANNEL")
+    FZMP_UnregisterMessageListener("FZX", receiveTransmission)
+end
 
-    if str == "version" then
-        -- Display all the current channels
-        cprint(L("fzxc version %s [%i]"):format(version, timestamp or 0))
-
-    elseif str == "info" then
-        -- Display all the current channels
-        cprint(L("fzxc: displaying all channels ..."))
-        updateSources()
-        for source, dests in pairs(sources) do
-            for dest, presenceIDs in pairs(dests) do
-                if source == dest then
-                    cprint(("* #%s"):format(source))
-                else
-                    cprint(("* #%s:%s"):format(source, dest))
-                end
-                for presenceID, info in pairs(presenceIDs) do
-                    local _, name =
-                        BNGetFriendInfo(BNGetFriendIndex(presenceID))
-                    local status
-                    if info.connected then
-                        status = ("|cff00ff00%s|r"):format(L("connected"))
+local slashCommands = {
+    version = {
+        description = L"Displays the version of the add-on.",
+        action = function()
+            cprintf(L"fzxc version %s [%i]", version, timestamp or 0)
+        end
+    },
+    info = {
+        description = L"Displays the current channels.",
+        action = function()
+            if FZXC_DB.disabled then
+                cprintf(L"fzxc: %s",
+                        string_format("|cffff6600%s|r", L"disabled"))
+                return
+            end
+            cprint(L"fzxc: displaying all channels ...")
+            updateSources()
+            for source, dests in pairs(sources) do
+                for dest, presenceIDs in pairs(dests) do
+                    if source == dest then
+                        cprintf("#%s", source)
                     else
-                        status = ("|cff999999%s|r"):format(L("not connected"))
+                        cprintf("#%s:%s", source, dest)
                     end
-                    cprint(("  - %s [%s]"):format(name, status))
+                    for presenceID, info in pairs(presenceIDs) do
+                        local _, name =
+                            BNGetFriendInfo(BNGetFriendIndex(presenceID))
+                        local status
+                        if info.connected then
+                            status = string_format("|cff00ff00%s|r",
+                                                   L"connected")
+                        else
+                            status = string_format("|cff999999%s|r",
+                                                   L"not connected")
+                        end
+                        cprintf("    %s [%s]", name, status)
+                    end
                 end
             end
         end
-
-    elseif str == "debug" then
-        DEBUG = not DEBUG
-        FZMP_DEBUG = DEBUG
-        cprint(("fzxc: debug %s"):format(DEBUG and "on" or "off"))
-
-    elseif str == "trace" then
-        if not DEBUG then
-            cprint("Debug mode must be on first!")
+    },
+    toggle = {
+        description = L"Enables / disables the add-on.",
+        action = function()
+            local state
+            if FZXC_DB.disabled then
+                enable()
+                state = string_format("|cff00ff00%s|r", L"enabled")
+            else
+                disable()
+                state = string_format("|cffff6600%s|r", L"disabled")
+            end
+            cprintf(L"fzxc: %s", state)
         end
-        dump(sources)
-
-    elseif str:sub(1, 5) == "ping " then
-        if not DEBUG then
-            cprint("Debug mode must be on first!")
+    },
+    enable = {
+        action = function()
+            local state
+            if FZXC_DB.disabled then
+                enable()
+                state = string_format("|cff00ff00%s|r", L"enabled")
+            else
+                state = string_format("|cff00ff00%s|r", L"already enabled")
+            end
+            cprintf(L"fzxc: %s", state)
         end
-        local presenceID = GetAutoCompletePresenceID(str:sub(6))
-        if presenceID then
-            FZMP_SendMessage(
-                "FZX",
-                {ECHO_REQUEST, tostring(GetTime()), ""},
-                "BN_WHISPER",
-                presenceID)
+    },
+    disable = {
+        action = function()
+            local state
+            if FZXC_DB.disabled then
+                state = string_format("|cffff6600%s|r", L"already disabled")
+            else
+                disable()
+                state = string_format("|cffff6600%s|r", L"disabled")
+            end
+            cprintf(L"fzxc: %s", state)
         end
+    },
+    debug = {
+        action = function()
+            DEBUG = not DEBUG
+            FZMP_DEBUG = DEBUG
+            cprintf("fzxc: debug %s", DEBUG and "on" or "off")
+        end
+    },
+    trace = {
+        action = function()
+            if not DEBUG then
+                cprint("Debug mode must be on first!")
+                return
+            end
+            dump(sources)
+        end
+    },
+    ping = {
+        action = function(name)
+            if not DEBUG then
+                cprint("Debug mode must be on first!")
+                return
+            end
+            local presenceID = GetAutoCompletePresenceID(name)
+            if presenceID then
+                FZMP_SendMessage(
+                    "FZX",
+                    {ECHO_REQUEST, tostring(GetTime()), ""},
+                    "BN_WHISPER",
+                    presenceID)
+            end
+        end
+    },
+}
 
-    else
-        local usage = {
-            L("Usage:"),
-            L("    /fzxc <command>"),
-            L("where <command> is one of:"),
-            L("    info"),
-            L("    version"),
-        }
-        for _, line in ipairs(usage) do
-            cprint(line)
+local function initialize()
+
+    -- Initialize constants
+    playerFaction = UnitFactionGroup("player")
+
+    -- Initialize saved variable
+    if not FZXC_DB then
+        FZXC_DB = {}
+    end
+    FZXC_DB.version = version
+    FZXC_DB.timestamp = timestamp
+
+    -- Start the message disposer
+    frame:SetScript("OnUpdate", onUpdate)
+
+    -- Query version of other player's addons
+    updateSources()
+    local connectedPresenceIDs = {}
+    for source, dests in pairs(sources) do
+        for dest, presenceIDs in pairs(dests) do
+            for presenceID, info in pairs(presenceIDs) do
+                if info.connected then
+                    connectedPresenceIDs[presenceID] = true
+                end
+            end
         end
     end
+    for presenceID, _ in pairs(connectedPresenceIDs) do
+        FZMP_SendMessage("FZX", {VERSION_REQUEST, version, "", timestamp},
+                         "BN_WHISPER", presenceID)
+    end
+
+    -- Set up slash commands
+    SLASH_FZXC1 = "/fzxc"
+    function SlashCmdList.FZXC(str)
+        local command, arguments = string_match(str, "([^%s]+)%s*(.*)")
+        local commandInfo = slashCommands[command]
+        if commandInfo then
+            commandInfo.action(arguments)
+            return
+        end
+
+        -- Display usage info if the command is not found
+        cprint(L"Usage:")
+        cprintf("    |cffffff00/fzxc|r |cff0099ff%s %s|r",
+                L"<command>", L"<arguments>")
+        cprintf(L"where %s is one of:",
+                string_format("|cff0099ff%s|r", L"<command>"))
+        for command, commandInfo in pairs(slashCommands) do
+            description = commandInfo.description
+            if description then
+                cprintf("    |cffffff00%s|r - %s", command, description)
+            end
+        end
+    end
+
 end
+
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript(
+    "OnEvent",
+    function(_, event, ...)
+        if event == "CHAT_MSG_CHANNEL" then
+            onChatMsgChannel(...)
+        elseif event == "PLAYER_ENTERING_WORLD" then
+            frame:UnregisterEvent("PLAYER_ENTERING_WORLD")
+            initialize()
+            if not FZXC_DB.disabled then
+                enable()
+            end
+        end
+    end)
